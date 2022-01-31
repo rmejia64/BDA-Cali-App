366857d33f8f61444408464ff8f7d385fef1ffe2
Arren Leung <akleung@scu.edu>
2022-01-30 21:38:52 -0800

    Pending donations filter

--- screens/donations/accepted/ViewScreen.js
+++ screens/donations/accepted/ViewScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
 import React from 'react';
 import { deleteDoc, doc, setDoc } from 'firebase/firestore';
 import { db } from '../../../firebase';
@@ -7,11 +7,16 @@ const ViewScreen = ({ route, navigation }) => {
     const { id, email, name } = route.params;
 
     const moveBack = async () => {
-        await setDoc(doc(db, 'donationForms', id), {
+        await setDoc(doc(db, 'pendingDonations', id), {
             name: name,
             email: email,
         });
-        await deleteDoc(doc(db, 'acceptedDonation', id));
+        await deleteDoc(doc(db, 'acceptedDonations', id));
+        navigation.goBack();
+    };
+
+    const handleDelete = async () => {
+        await deleteDoc(doc(db, 'acceptedDonations', id));
         navigation.goBack();
     };
 
@@ -20,8 +25,27 @@ const ViewScreen = ({ route, navigation }) => {
             <Text>id: {id}</Text>
             <Text>email: {email}</Text>
             <Text>name: {name}</Text>
-            <TouchableOpacity onPress={moveBack}>
-                <Text>Move back to pending</Text>
+            <TouchableOpacity
+                onPress={() => {
+                    Alert.alert(
+                        'Accept',
+                        'Are you sure you want to move this donation to pending?',
+                        [{ text: 'No' }, { text: 'Yes', onPress: moveBack }]
+                    );
+                }}
+            >
+                <Text>Move to pending</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {
+                    Alert.alert(
+                        'Accept',
+                        'Are you sure you want to delete this donation?',
+                        [{ text: 'No' }, { text: 'Yes', onPress: handleDelete }]
+                    );
+                }}
+            >
+                <Text>Delete</Text>
             </TouchableOpacity>
         </View>
     );
--- screens/donations/pending/ViewScreen.js
+++ screens/donations/pending/ViewScreen.js
@@ -14,7 +14,7 @@ const ViewScreen = ({ route, navigation }) => {
         });
 
         // delete donation from donationsForms database
-        await deleteDoc(doc(db, 'donationForms', id));
+        await deleteDoc(doc(db, 'pendingDonations', id));
 
         // automatically navigate back to list view
         navigation.goBack();
@@ -26,49 +26,76 @@ const ViewScreen = ({ route, navigation }) => {
          * donations for recovery purposes
          */
         // await setDoc(doc(db, 'deletedDonations', id), {});
-        await deleteDoc(doc(db, 'donationForms', id));
+        await deleteDoc(doc(db, 'pendingDonations', id));
 
         // automatically navigate back to list view
         navigation.goBack();
     };
 
     return (
-        <View>
+        <View style={styles.container}>
             <Text>id: {id}</Text>
             <Text>email: {email}</Text>
             <Text>name: {name}</Text>
-            <TouchableOpacity
-                onPress={() => {
-                    Alert.alert(
-                        'Accept',
-                        'Are you sure you want to accept this donation?',
-                        [
-                            { text: 'No' },
-                            { text: 'Yes', onPress: acceptDonation },
-                        ]
-                    );
-                }}
-            >
-                <Text>Accept</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => {
-                    Alert.alert(
-                        'Delete',
-                        'Are you sure you want to delete this donation?',
-                        [
-                            { text: 'No' },
-                            { text: 'Yes', onPress: deleteDonation },
-                        ]
-                    );
-                }}
-            >
-                <Text>Delete</Text>
-            </TouchableOpacity>
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        Alert.alert(
+                            'Accept',
+                            'Are you sure you want to accept this donation?',
+                            [
+                                { text: 'No' },
+                                { text: 'Yes', onPress: acceptDonation },
+                            ]
+                        );
+                    }}
+                >
+                    <Text style={styles.buttonText}>Accept</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        Alert.alert(
+                            'Delete',
+                            'Are you sure you want to delete this donation?',
+                            [
+                                { text: 'No' },
+                                { text: 'Yes', onPress: deleteDonation },
+                            ]
+                        );
+                    }}
+                >
+                    <Text style={styles.buttonText}>Delete</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 export default ViewScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    buttonsContainer: {
+        width: '100%',
+        paddingTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    button: {
+        backgroundColor: '#0c4484',
+        width: '40%',
+        padding: 15,
+        borderRadius: 15,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '500',
+        fontSize: 16,
+    },
+});
