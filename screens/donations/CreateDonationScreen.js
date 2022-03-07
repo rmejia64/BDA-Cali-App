6630e3c2005224adeefb204e668a8d487c063a13
Adrian Ramirez <96157580+adrianramirez-03@users.noreply.github.com>
2022-03-06 23:22:28 -0800

    created in app form
    made in app form, will update fields and values once we get more details to better suit the bank's form structure

--- screens/donations/CreateDonationScreen.js
+++ screens/donations/CreateDonationScreen.js
@@ -1,14 +1,297 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Image, Button, SafeAreaView, TextInput, ScrollView} from 'react-native';
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useForm, Controller} from 'react-hook-form';
+
 
 const CreateDonationScreen = () => {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    );
+    const { control, handleSubmit, resetField, formState: { errors } } = useForm({
+        defaultValues: {
+          type: '',
+          address: '',
+          certificate: '',
+          productType: '',
+          packaging: '',
+          quantity: '',
+          weight: '',
+        }
+      });
+      const onSubmit = data => console.log(data);
+
+      const handleClear = () => {
+            resetField("type"),
+            resetField("address"),
+            resetField("certificate"),
+            resetField("productType"),
+            resetField("packaging"),
+            resetField("quantity"),
+            resetField("weight")
+        };
+
+
+      return (
+        <SafeAreaView>
+            <ScrollView>
+                <View>
+                    <Text style={styles.mainHeader}> New Form</Text>
+                </View>
+
+                <View style={styles.section}>
+                    <View style={styles.rowContainer}>
+                        <Text style={styles.topHeader}> A. General Information </Text>
+                    </View>
+                    <Text style={styles.infoHeader}>Type of Client:</Text>
+                    <View style={styles.entry}>
+                        <Controller
+                            style={styles.entry}
+                            control={control}
+                            rules={{
+                                required: true,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={styles.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                placeholder='Individual or Organization'
+                            />
+                            )}
+                            name="type"
+                        />
+                        {errors.type && <Text style={styles.error}>This is required.</Text>}
+                    </View>
+                
+                    <Text style={styles.infoHeader}>Address:</Text>
+                    <View style={styles.entry}>
+                        <Controller
+                            style={styles.entry}
+                            control={control}
+                            rules={{
+                                required: true,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={styles.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                placeholder='Address'
+                            />
+                            )}
+                            name="address"
+                        />
+                        {errors.address && <Text style={styles.error}>This is required.</Text>}
+                    </View>
+
+                    <Text style={styles.infoHeader}>Certificate Required?</Text>
+                    <View style={styles.entry}>
+                        <Controller
+                            style={styles.entry}
+                            control={control}
+                            rules={{
+                                required: true,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={styles.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                placeholder='Yes/No'
+                            />
+                            )}
+                            name="certificate"
+                        />
+                        {errors.certificate && <Text style={styles.error}>This is required.</Text>}
+                    </View>
+
+
+                    <Text style={styles.header}> B. Donation Information </Text>
+                    <Text style={styles.infoHeader}>Type of Product</Text>
+                    <View style={styles.entry}>
+                        <Controller
+                            style={styles.entry}
+                            control={control}
+                            rules={{
+                                required: true,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={styles.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                placeholder='Perishable/Non-Perishable'
+                            />
+                            )}
+                            name="productType"
+                        />
+                        {errors.productType && <Text style={styles.error}>This is required.</Text>}
+                    </View>
+
+                    <Text style={styles.infoHeader}>Packaging Type</Text>
+                    <View style={styles.entry}>
+                        <Controller
+                            style={styles.entry}
+                            control={control}
+                            rules={{
+                                required: true,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={styles.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                placeholder='Type of Packaging'
+                            />
+                            )}
+                            name="packaging"
+                        />
+                        {errors.packaging && <Text style={styles.error}>This is required.</Text>}
+                    </View>
+
+                    <Text style={styles.infoHeader}>Quantity</Text>
+                    <View style={styles.entry}>
+                        <Controller
+                            style={styles.entry}
+                            control={control}
+                            rules={{
+                                required: true,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={styles.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                placeholder='e.g. # of boxes'
+                            />
+                            )}
+                            name="quantity"
+                        />
+                        {errors.quantity && <Text style={styles.error}>This is required.</Text>}
+                    </View>
+
+                    <Text style={styles.infoHeader}>Weight</Text>
+                    <View style={styles.entry}>
+                        <Controller
+                            style={styles.entry}
+                            control={control}
+                            rules={{
+                                required: true,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={styles.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                placeholder='In Kilos'
+                            />
+                            )}
+                            name="weight"
+                        />
+                        {errors.weight && <Text style={styles.error}>This is required.</Text>}
+                    </View>
+
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity onPress={handleSubmit(onSubmit)} style={styles.button}>
+                            <Text style={styles.buttonText}>Submit</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.clearButton}>
+                        <TouchableOpacity onPress={handleClear} style={styles.button}>
+                            <Text style={styles.clearText}>Clear</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+      );
 };
+
 
 export default CreateDonationScreen;
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+    rowContainer: {
+        flex: 1,
+    },
+    topHeader: {
+        fontFamily: 'Helvetica Neue',
+        fontSize: 20,
+        paddingTop: 30,
+    },
+    clearButton: {
+        paddingBottom: 15,
+        paddingHorizontal: 110,
+    },
+    clearText: {
+        fontSize: 18,
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+        textTransform: "uppercase"
+
+    },
+    error: {
+        color: 'red',
+        paddingLeft: 4,
+    },
+    section: {
+        paddingTop: 0,
+    },
+    mainHeader: {
+        fontFamily: 'Helvetica-Bold',
+        fontSize: 40,
+        paddingTop: 25,
+        justifyContent: 'center',
+        textAlign: 'center',
+        
+    },
+    buttonContainer: {
+        paddingVertical: 25,
+        paddingHorizontal: 110,
+    },
+    button: {
+        elevation: 8,
+        backgroundColor: "#186ae7",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 12
+    },
+    buttonText: {
+        fontSize: 18,
+        color: "#fff",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase"
+    },
+    header: {
+        fontFamily: 'Helvetica Neue',
+        fontSize: 20,
+        paddingTop: 30,
+    },
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 15,
+        borderRadius: 15,
+        marginTop: 10,
+        
+    },
+    infoHeader: {
+        paddingTop: 10,
+        paddingLeft: 5,
+    },
+    entry: {
+        paddingHorizontal: 5,
+    },
+
+    
+});
+
+const {height} = Dimensions.get("screen");
